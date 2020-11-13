a9076d979417196f33d7422a44d4329f58dc5742
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-13 13:34:38 +0000

    Create guessNumber function taking player inputs and couting game score.

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php
-    //generate a random number between 1 and 10. You’ll run the game 10 times and tell the user some information about their guessing abilities.
+<?php   //generate a random number between 1 and 10. Run the game n times and tell the user some information about their guessing abilities.
+//initial global variables: 
+$play_count = 0; 
+$correct_guesses = 0;
+$guess_high = 0;
+$guess_low = 0;
+
+echo "Try to guess a number from 1 to 10 inclusive.\n"; 
+
+function guessNumber(){
+  global $play_count, $correct_guesses, $guess_high, $guess_low;  
+  $play_count++; // etermine round
+  $rand_num = rand(1,10);  // PCU choice
+  
+  echo "\nMake a guess...\n"; 
+  $player_guess = readline(">> ");
+  $player_guess = intval($player_guess); //converting to number type
+  echo "\nROUND: $play_count\n\nYour guess: $player_guess\nAI pick: $rand_num\n"; 
+
+  //logic counting game score: 
+  if($player_guess === $rand_num){
+    $correct_guesses++; 
+  }elseif($player_guess > $rand_num){
+    $guess_high++;
+  }elseif($player_guess < $rand_num){
+    $guess_low++;
+  } 
+
+}
+
+guessNumber(); 
